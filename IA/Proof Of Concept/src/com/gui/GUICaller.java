198e198dcee2e5492ad5475f128f32444cb6ceae
Lissan Koirala <58141138+LissanKoirala@users.noreply.github.com>
2022-05-05 10:09:00 +0100

    Login GUI Added

--- IA/Proof Of Concept/src/com/gui/GUICaller.java
+++ IA/Proof Of Concept/src/com/gui/GUICaller.java
@@ -10,10 +10,21 @@ public class GUICaller {
 
     public void StockChooser(){
         // demo GUI
-        System.out.println("SEQUENCE: Program started");
-        JFrame frame = new JFrame("Demo frame");
+        System.out.println("SEQUENCE: Stock Chooser");
+        JFrame frame = new JFrame("Stock");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         StockChooser myGUI = new StockChooser(600, 400);
+        frame.add(myGUI);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void Login(){
+        // demo GUI
+        System.out.println("SEQUENCE: Login");
+        JFrame frame = new JFrame("Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Login myGUI = new Login(350, 150);
         frame.add(myGUI);
         frame.pack();
         frame.setVisible(true);
---
+++ IA/Proof Of Concept/src/com/gui/Login.java
@@ -0,0 +1,62 @@
+package com.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JPanel implements ActionListener{
+    // canvas for other GUI widgets
+
+    String username = "admin";
+    String password = "admin";
+
+    JButton button;
+    JLabel usernameLabel;
+    JTextField usernameTextField;
+    JLabel passwordLabel;
+    JPasswordField passwordField;
+
+    public Login(int width, int height) {
+        System.out.println("SEQUENCE: GUI constructor");
+        this.setPreferredSize(new Dimension(width, height));
+        setLayout(null);
+
+        usernameLabel = new JLabel("Username");
+        usernameLabel.setBounds(40,20, 80, 25);
+
+        usernameTextField = new JTextField();
+        usernameTextField.setBounds(130,20, 165, 25);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(40,50, 80, 25);
+
+        passwordField = new JPasswordField();
+        passwordField.setBounds(130,50, 165, 25);
+
+        button = new JButton("Submit");
+        button.addActionListener(this);
+        button.setBounds(40,85, 80, 25);
+
+        add(usernameLabel);
+        add(usernameTextField);
+        add(passwordLabel);
+        add(passwordField);
+        add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String inputUsername = usernameTextField.getText();
+        String inputPassword = passwordField.getText();
+
+        if (inputUsername.equals(username) && inputPassword.equals(password)){
+            System.out.print("User Authenticated...");
+
+        } else { // if authentication credentials invalid...
+            JOptionPane.showMessageDialog(this, "Invalid Credentials, Try Again");
+            passwordField.setText(""); // Clears the password field...
+        }
+
+    }
+}
