64679b72a125734a2ea5c37ae0e1fa930bd1169a
Lissan Koirala <58141138+LissanKoirala@users.noreply.github.com>
2022-05-17 11:47:35 +0100

    +Inheritance, stocks, crypto & forex inherit from asset class

---
+++ IA/Proof Of Concept/src/com/stock/Crypto.java
@@ -0,0 +1,9 @@
+package com.stock;
+
+public class Crypto extends Stock {
+
+    // TODO: Change the ticker symbol to the format it needs to be in...
+    public Crypto(String ticker) throws Exception {
+        super(ticker);
+    }
+}
---
+++ IA/Proof Of Concept/src/com/stock/Forex.java
@@ -0,0 +1,8 @@
+package com.stock;
+
+public class Forex extends Stock {
+
+    public Forex(String ticker) throws Exception {
+        super(ticker);
+    }
+}
