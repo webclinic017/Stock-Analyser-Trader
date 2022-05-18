63569ee2918e10997c0cc3be4929902d68f75058
Lissan Koirala <lissanartist@gmail.com>
2022-05-18 18:17:14 +0100

    Added custom stock chart graph

--- IA/Proof Of Concept/src/com/gui/StockInfo.java
+++ IA/Proof Of Concept/src/com/gui/StockInfo.java
@@ -19,6 +19,8 @@ import java.nio.file.StandardCopyOption;
 public class StockInfo extends JPanel {
     // canvas for other GUI widgets
 
+    Asset asset;
+
     FileHandler FileHandler = new FileHandler();
 
     JButton button;
@@ -27,6 +29,8 @@ public class StockInfo extends JPanel {
 
     // TODO: Add a iframe and embed tradingview 
     public StockInfo(int width, int height, Asset asset) throws IOException {
+        this.asset = asset;
+
         this.setPreferredSize(new Dimension(width, height));
         setLayout(null);
 
@@ -40,20 +44,19 @@ public class StockInfo extends JPanel {
 
         // TODO: Implement my own in python and open an api endpoint...
         // Options : [1m,3m,5m,15m,30m,45m,1h,2h,3h,4h,1d,1w]
-        String url = "https://api.chart-img.com/v1/tradingview/advanced-chart?interval=1d&height=550&width=800&theme=light&format=png&key=00e94094-4d2c-4658-b32e-776be716517b&symbol="+asset.ticker;
-
-        File directory = new File("data/temp/");
-        if (! directory.exists()){
-            directory.mkdirs();
-        }
-
-        try (InputStream in = new URL(url).openStream()) {
-            Files.copy(in, Paths.get("data/temp/chart.png"), StandardCopyOption.REPLACE_EXISTING);
-        }
-
-        chart = new JLabel(new ImageIcon(new ImageIcon("data/temp/chart.png").getImage().getScaledInstance(560, 385, Image.SCALE_SMOOTH)));  // scaling the image properly so that there is no stretch
-        chart.setBounds(20,200, 560, 385);
-
+//        String url = "https://api.chart-img.com/v1/tradingview/advanced-chart?interval=1d&height=550&width=800&theme=light&format=png&key=00e94094-4d2c-4658-b32e-776be716517b&symbol="+asset.ticker;
+//
+//        File directory = new File("data/temp/");
+//        if (! directory.exists()){
+//            directory.mkdirs();
+//        }
+//
+//        try (InputStream in = new URL(url).openStream()) {
+//            Files.copy(in, Paths.get("data/temp/chart.png"), StandardCopyOption.REPLACE_EXISTING);
+//        }
+//
+//        chart = new JLabel(new ImageIcon(new ImageIcon("data/temp/chart.png").getImage().getScaledInstance(560, 385, Image.SCALE_SMOOTH)));  // scaling the image properly so that there is no stretch
+//        chart.setBounds(20,200, 560, 385);
 
 
 
@@ -78,6 +81,27 @@ public class StockInfo extends JPanel {
 
         add(name);
         add(icon);
-        add(chart);
+//        add(chart);
     }
+
+
+    // TODO: make a algorithm that limits the lines from shooting off the graph, limit the max x and y values according to the prices overtime
+    // TODO: for eg: BTC's price of 30,000 shoots off the graph... make it relative, probably graph the percentage increase?
+//    public void paint(Graphics g){ // paints the stock chart
+//        Float[][] historical_data = asset.historical_data;
+//
+//        int base_number = 600;
+//
+//        float previous_close = historical_data[0][5]; // getting the first data point as previous close so that it doesn't start from 0
+//        int day_counter = 0;
+//
+//        for (Float[] daily_data : historical_data) {
+//            float close_price = daily_data[5]; // 5 for close price
+//
+//            g.drawLine(day_counter, base_number-(int) previous_close, day_counter+3, base_number-(int) close_price); // TODO: adjust the +2 based on the number of data points
+//
+//            previous_close = close_price;
+//            day_counter++;
+//        }
+//    }
 }
