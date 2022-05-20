f60b589d09a63c88cf40d9cf1f124077b6b566d3
Lissan Koirala <58141138+LissanKoirala@users.noreply.github.com>
2022-05-20 11:43:42 +0100

    Added some ideas

--- IA/Proof Of Concept/src/com/gui/StockInfo.java
+++ IA/Proof Of Concept/src/com/gui/StockInfo.java
@@ -10,8 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.Dimension;
-import java.awt.Image;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -48,19 +47,19 @@ public class StockInfo extends JPanel {
 
         // TODO: Implement my own in python and open an api endpoint...
         // Options : [1m,3m,5m,15m,30m,45m,1h,2h,3h,4h,1d,1w]
-//        String url = "https://api.chart-img.com/v1/tradingview/advanced-chart?interval=1d&height=550&width=800&theme=light&format=png&key=00e94094-4d2c-4658-b32e-776be716517b&symbol="+asset.ticker;
-//
-//        File directory = new File("data/temp/");
-//        if (! directory.exists()){
-//            directory.mkdirs();
-//        }
-//
-//        try (InputStream in = new URL(url).openStream()) {
-//            Files.copy(in, Paths.get("data/temp/chart.png"), StandardCopyOption.REPLACE_EXISTING);
-//        }
-//
-//        chart = new JLabel(new ImageIcon(new ImageIcon("data/temp/chart.png").getImage().getScaledInstance(560, 385, Image.SCALE_SMOOTH)));  // scaling the image properly so that there is no stretch
-//        chart.setBounds(20,200, 560, 385);
+        String url = "https://api.chart-img.com/v1/tradingview/advanced-chart?interval=1d&height=550&width=800&theme=light&format=png&key=00e94094-4d2c-4658-b32e-776be716517b&symbol="+asset.ticker;
+
+        File directory = new File("data/temp/");
+        if (! directory.exists()){
+            directory.mkdirs();
+        }
+
+        try (InputStream in = new URL(url).openStream()) {
+            Files.copy(in, Paths.get("data/temp/chart.png"), StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        chart = new JLabel(new ImageIcon(new ImageIcon("data/temp/chart.png").getImage().getScaledInstance(560, 385, Image.SCALE_SMOOTH)));  // scaling the image properly so that there is no stretch
+        chart.setBounds(20,200, 560, 385);
 
 
 
@@ -85,12 +84,14 @@ public class StockInfo extends JPanel {
 
         add(name);
         add(icon);
-//        add(chart);
+        add(chart);
     }
 
 
     // TODO: make a algorithm that limits the lines from shooting off the graph, limit the max x and y values according to the prices overtime
     // TODO: for eg: BTC's price of 30,000 shoots off the graph... make it relative, probably graph the percentage increase?
+    // TODO: find the highest and lowest point, plot the percentage increase
+    // TODO: just find the higest point and divide everything by it...
 //    public void paint(Graphics g){ // paints the stock chart
 //        Float[][] historical_data = asset.historical_data;
 //
