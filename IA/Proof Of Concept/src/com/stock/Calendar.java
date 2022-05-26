254d4a4e0308670295e783c7b6ed45b1de003b16
Lissan Koirala <58141138+LissanKoirala@users.noreply.github.com>
2022-05-26 10:15:53 +0100

    Clock returns if market open + next close or vice versa

--- IA/Proof Of Concept/src/com/stock/Calendar.java
+++ IA/Proof Of Concept/src/com/stock/Calendar.java
@@ -10,6 +10,21 @@ public class Calendar {
     public void isMarketOpen() throws Exception {
         AlpacaAPIHandler.clock();
     }
+    
+    // returns is_open=true: next_close, or is_open=false: next_open 
+    public String[] isMarketOpen() throws Exception {
+        JsonObject response = AlpacaAPIHandler.clock().get(0).getAsJsonObject();
+        String is_open = response.get("is_open").getAsString();
+
+        String next_open = response.get("next_open").getAsString();
+        String next_close = response.get("next_close").getAsString();
+
+        if (is_open.equals("true")){
+            return new String[]{is_open, next_close};
+        } else {
+            return new String[]{is_open, next_open};
+        }
+    }
 }
 
 // TODO: https://finnhub.io/docs/api/earnings-calendar
