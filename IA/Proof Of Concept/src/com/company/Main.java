d8c671191fb825fcbe4fa962d7e23537ddde7f05
Lissan Koirala <58141138+LissanKoirala@users.noreply.github.com>
2022-08-07 10:33:03 +0100

    Idea to separate data into training and testing

--- IA/Proof Of Concept/src/com/company/Main.java
+++ IA/Proof Of Concept/src/com/company/Main.java
@@ -17,6 +17,8 @@ import javax.swing.*;
 import java.io.File;
 import java.util.Arrays;
 
+// TODO: FIND A WAY TO SEPARATE THE DATA INTO TRAINING AND TESTING - INCLUDE IN CRITERIONS AND EMAIL COMMUNICATIONS...
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
