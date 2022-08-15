3e328c68756f0502241753c0868cd6d9defaec4c
Lissan Koirala <58141138+LissanKoirala@users.noreply.github.com>
2022-08-15 22:42:07 +0100

    Added Flask server to execute trades with requests from Java

--- IA/Proof Of Concept/Python/AlpacaDataStream.py
+++ IA/Proof Of Concept/Python/AlpacaDataStream.py
@@ -24,3 +24,25 @@ stream = Stream("PKZLW20DRB2DJFPPRMZV",
 stream.subscribe_quotes(quote_callback, 'AAPL')
 stream.subscribe_news(news_callback, '*')
 stream.run()
+
+
+
+# create a flask server with a url endpoint to execute trades
+# from flask import Flask, request
+# app = Flask(__name__)
+#
+# @app.route('/trade', methods=['POST'])
+# def trade():
+#     trade_type = request.form['trade_type']
+#     symbol = request.form['symbol']
+#     qty = request.form['qty']
+#     side = request.form['side']
+#     time_in_force = request.form['time_in_force']
+#     order_class = request.form['order_class']
+#     take_profit = request.form['take_profit']
+#     stop_loss = request.form['stop_loss']
+#     trailing_stop = request.form['trailing_stop']
+
+#     stream.execute_trade(trade_type=trade_type, symbol=symbol, qty=qty, side=side, time_in_force=time_in_force, order_class=order_class, take_profit=take_profit, stop_loss=stop_loss, trailing_stop=trailing_stop)
+#     return 'trade executed'
+#
