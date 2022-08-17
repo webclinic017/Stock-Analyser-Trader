c676bdcec10906d0f10ae8314a35ce84ac67c903
Lissan Koirala <58141138+LissanKoirala@users.noreply.github.com>
2022-08-17 13:36:51 +0100

    Executing trades successfully

--- IA/Proof Of Concept/Python/AlpacaDataStream.py
+++ IA/Proof Of Concept/Python/AlpacaDataStream.py
@@ -1,48 +1,51 @@
-# https://colab.research.google.com/drive/1MF_tbivSvSfXvLRLJifDzkQ_8FjWukYy?usp=sharing#scrollTo=fLnJLSLQnTxV
-
+import alpaca_trade_api as tradeapi
 from alpaca_trade_api.stream import Stream
 from alpaca_trade_api.common import URL
+from flask import Flask, request
 
-async def trade_callback(t):
-    print('trade', t)
+
+app = Flask(__name__)
+
 
 async def quote_callback(q):
     print('quote', q)
 
-async def news_callback(q):
-    # TODO: Add a parser that parses any html tags that it may contain as it's a stream, might not have been yet removed.
-    print('news', q)
 
-# Initiate Class Instance
-stream = Stream("PKZLW20DRB2DJFPPRMZV",
-                "HS655suik2zGGRFXbqe5b7Xc1GRT8jHFVxQxzD1g",
-                base_url=URL('https://paper-api.alpaca.markets'),
-                data_feed='iex')  # <- replace to 'sip' if you have PRO subscription
+api_key = "PKBRQ877H23MLZ6A5A44"
+api_secret = "kYASo3caUfQ6yRdgMLC72aFkaXo7T7K9mCIK9pRa"
+base_url = 'https://paper-api.alpaca.markets'
+
+stream = Stream(api_key,
+                api_secret,
+                base_url=URL(base_url),
+                data_feed='iex') 
+
 
 # subscribing to event
-#stream.subscribe_trades(trade_callback, 'AAPL')
-stream.subscribe_quotes(quote_callback, 'AAPL')
-stream.subscribe_news(news_callback, '*')
+# stream.on_bar(quote_callback, 'AAPL')
+
+stream.on_crypto_bar(quote_callback, 'BTCUSD')
+
 stream.run()
 
 
+api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
 
-# create a flask server with a url endpoint to execute trades
-# from flask import Flask, request
-# app = Flask(__name__)
-#
-# @app.route('/trade', methods=['POST'])
-# def trade():
-#     trade_type = request.form['trade_type']
-#     symbol = request.form['symbol']
-#     qty = request.form['qty']
-#     side = request.form['side']
-#     time_in_force = request.form['time_in_force']
-#     order_class = request.form['order_class']
-#     take_profit = request.form['take_profit']
-#     stop_loss = request.form['stop_loss']
-#     trailing_stop = request.form['trailing_stop']
+@app.route('/trade', methods=['GET'])
+def trade():
+    symbol = request.args.get('symbol')
+    qty = request.args.get('qty')
+    side = request.args.get('side')
 
-#     stream.execute_trade(trade_type=trade_type, symbol=symbol, qty=qty, side=side, time_in_force=time_in_force, order_class=order_class, take_profit=take_profit, stop_loss=stop_loss, trailing_stop=trailing_stop)
-#     return 'trade executed'
-#
+    try:
+        answer = api.submit_order(symbol, qty=qty, side=side, time_in_force='gtc')
+        return "Success"
+    except Exception as e:
+        return str(e)    
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
